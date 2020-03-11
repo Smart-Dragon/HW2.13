7652ea8b9f300e26e6d5994520bd5241ef9190b1
Aleksei <supportalex@rambler.ru>
2020-03-12 00:21:30 +0300

    fix

--- HW2.13/Extensions/UIColor.swift
+++ HW2.13/Extensions/UIColor.swift
@@ -10,11 +10,11 @@ import UIKit
 
 extension UIColor {
     
-    public convenience init(r: Int, g: Int, b: Int, a: CGFloat = 1) {
+    convenience init(r: Int, g: Int, b: Int, a: CGFloat = 1) {
         self.init(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: a)
     }
     
-    public convenience init(gray: Int, alpha: CGFloat) {
+    convenience init(gray: Int, alpha: CGFloat) {
         self.init(red: CGFloat(gray)/255, green: CGFloat(gray)/255, blue: CGFloat(gray)/255, alpha: alpha)
     }
     
